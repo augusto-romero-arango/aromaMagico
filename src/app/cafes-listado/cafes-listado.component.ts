5d1cb7ec66823afacc1fa63b07eed252cf16b481
Augusto Romero <a.romeroa23@uniandes.edu.co>
2023-11-04 19:52:24 -0500

    se crea la página principal de la aplicación

---
+++ src/app/cafes-listado/cafes-listado.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cafes-listado',
+  templateUrl: './cafes-listado.component.html',
+  styleUrls: ['./cafes-listado.component.css']
+})
+export class CafesListadoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
